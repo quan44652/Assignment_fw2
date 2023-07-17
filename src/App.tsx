48a0036988d51a5f2a3368455c1ea794ab7e7316
hiepnvph28430 <mrhiep165@gmail.com>
2023-07-18 01:25:13 +0700

    page detail product

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Home from "./Pages/Home";
+import Category from "./Components/Category/Category";
+import Footer from "./Components/Footer/Footer";
+import Product from "./Components/Product/Product";
+import Detail from "./Pages/Detail/Detail";
+
 
 function App() {
   return (
     <div>
-
+      <Detail />
     </div>
   );
 }
