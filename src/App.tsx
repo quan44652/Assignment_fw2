7cf2664ee62cb67004e834a20a565fad800a4e29
Lê Sỹ Hải <116716667+LeeHairDev@users.noreply.github.com>
2023-07-19 00:44:32 +0700

    create-layout-logic-auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { getAllCategories, getCategoryById } from "./apis/category";
@@ -9,22 +11,24 @@ import Top_navigate from "./Components/Top_navigate/Top_navigate";
 import { ICategory } from "./interfaces/category";
 import { IProduct } from "./interfaces/product";
 import Detail from "./Pages/Detail/Detail";
+import SignIn from "./Components/Signin/signin.component";
+import { Iauth } from "./interfaces/auth";
+import { addAccount } from "./apis/auth";
+import SignUpForm from "./Components/Signup/signup.component";
 
 
 function App() {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [categories, setCategory] = useState<ICategory[]>([]);
+  const [dataAuth, setData] = useState<Iauth[]>([]);
   const [selectedCategoryId, setSelectedCategoryId] = useState(null);
   useEffect(() => {
     getAllProducts().then(({ data }) => setProducts(data))
     getAllCategories().then(({ data }) => setCategory(data))
   }, [])
-
-
-
-
-
-
+  const onHandleAddAccount = (data: Iauth) => {
+     addAccount(data).then(() => setData([...dataAuth, data]));
+  };
   return (
     <div className="App">
       <Top_navigate />
@@ -36,6 +40,11 @@ function App() {
             <Route path=':id' element={<Detail products={products} />} />
           </Route>
         </Route>
+      </Routes>
+      <Routes>
+        
+          <Route path='/signin'  element={<SignIn />}/>
+          <Route path='/signup'  element={<SignUpForm onAdd={onHandleAddAccount}/>}/>
       </Routes>
       <Footer />
     </div>
