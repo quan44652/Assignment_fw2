e59ccadcdcd5aee3e4f5f1d0b2b7666e0bc42c92
Lê Sỹ Hải <116716667+LeeHairDev@users.noreply.github.com>
2023-07-21 00:58:18 +0700

    update

--- src/Components/Top_navigate/Top_navigate.tsx
+++ src/Components/Top_navigate/Top_navigate.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from "./Top_navigate.module.scss"
-const Top_navigate = () => {
-    return (
-        <div className={styles.top_navigate}>
-            <ul>
-                <li><a href="">Bản mobile</a></li>
-                <li><a href="">Giới thiệu</a></li>
-                <li><a href="">Sản phẩm đã xem</a></li>
-                <li><a href="">Trung tâm bảo hành</a></li>
-                <li><a href="">Hệ thống 126 siêu thị</a></li>
-                <li><a href="">Tuyển dụng</a></li>
-                <li><a href="">Tra cứu đơn hàng</a></li>
-                <li><a ><Link to={"signin"}>Đăng nhập</Link></a></li>
-            </ul>
-        </div >
-    )
-}
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Top_navigate.module.scss";
 
-export default Top_navigate
+const Top_navigate = () => {
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  return (
+    <div className={styles.top_navigate}>
+      <ul>
+        <li>
+          <a href="">Bản mobile</a>
+        </li>
+        <li>
+          <a href="">Giới thiệu</a>
+        </li>
+        <li>
+          <a href="">Sản phẩm đã xem</a>
+        </li>
+        <li>
+          <a href="">Trung tâm bảo hành</a>
+        </li>
+        <li>
+          <a href="">Hệ thống 126 siêu thị</a>
+        </li>
+        <li>
+          <a href="">Tuyển dụng</a>
+        </li>
+        <li>
+          <a href="">Tra cứu đơn hàng</a>
+        </li>
+        {user ? (
+          <li style={{ padding: "0 20px", color: "white" }}>{user.name}</li>
+        ) : (
+          <li>
+            <a>
+              <Link to={"signin"}>Đăng nhập</Link>
+            </a>
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default Top_navigate;
